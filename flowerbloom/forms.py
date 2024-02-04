5adb1473271649fb7dcc057f5cd2695d2055d22b
Anoushka Priya <anoushkapr202@gmail.com>
2024-02-04 23:50:24 +0530

    Sample Change

--- flowerbloom/forms.py
+++ flowerbloom/forms.py
@@ -1,9 +1,14 @@
 #flowerbloom/forms.py
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from . models import MyUser
 
 class MyRegFrm(UserCreationForm):
     class Meta:
         model = MyUser
         fields = ("username", "first_name", "last_name", "email")
+
+class LoginFrm(AuthenticationForm):
+    class Meta:
+        model = MyUser
+        fields = ("email")
--- flowerbloom/models.py
+++ flowerbloom/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
-
 class MyUser(AbstractUser):
     mobile=models.CharField(max_length=12,verbose_name='Phone Number')
     address=models.TextField(verbose_name='Address')
@@ -14,23 +13,24 @@ class Flower(models.Model)  :
 class Category(models.Model):
     cat_id=models.AutoField(primary_key=True)
     cat_name=models.CharField(max_length=50,verbose_name='Category name')
-    def __str__(self):
+    def _str_(self):
         return self.cat_name
+    
 class Product(models.Model):
     p_id=models.AutoField(primary_key=True)
     name=models.CharField(max_length=150,verbose_name='product name')
     description=models.TextField()
     price=models.DecimalField(max_digits=10,decimal_places=2)
-    product_info = models.TextField()
+    image=models.ImageField(upload_to='products/')
+    category=models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products', verbose_name='Category name')
     delivery_info = models.TextField()
     delivery_charge = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Delivery Charge')
-    image=models.ImageField(upload_to='products/')
-    category=models.ForeignKey(Category, on_delete=models.CASCADE,verbose_name='Category name')
+    product_info=models.TextField()
 
-    def __str__(self):
+
+    def _str_(self):
         return self.name
-    
-    class Cart(models.Model):
+class Cart(models.Model):
     product=models.ForeignKey(Product,on_delete=models.CASCADE)
     quantity=models.PositiveIntegerField(default=0)
     user=models.ForeignKey(MyUser,on_delete=models.CASCADE)
@@ -38,3 +38,5 @@ class Product(models.Model):
 
     def _str_(self):
         return f'{self.quantity} x {self.product.name}'
+    
+    
